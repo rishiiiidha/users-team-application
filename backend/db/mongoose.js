449ef156f9038e2a9fac1982968c80d3c29d2434
rishidha <rishidha04@gmail.com>
2024-04-02 23:50:07 +0530

    updated

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,15 +1,29 @@
-const mongoose = require('mongoose');
-const User = require('../models/user')
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const User = require("../models/user");
 
-const connection = async (req, res) =>{
-    try{
-        await mongoose.connect('mongodb://localhost:27017/team-app');
-        console.log('successfully connected to mongodb');
-    }
-    catch(e){
-        console.log(`unable to connect to mongodb : ${e.message}`);
-    }
-}
+const connection = async (req, res) => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://rishidha04:ILu5sLmjsqim7AQy@cluster0.rtbw5ul.mongodb.net/team-app"
+    );
+    console.log("Successfully connected to MongoDB Atlas");
+
+    // Read data from JSON file
+    // const jsonData = fs.readFileSync(
+    //   path.resolve(__dirname, "data.json"),
+    //   "utf-8"
+    // );
+    // const data = JSON.parse(jsonData);
+
+    // // Insert data into the database
+    // await User.insertMany(data);
+    // console.log("Data imported successfully.");
+  } catch (e) {
+    console.log(`Error importing data to MongoDB Atlas: ${e.message}`);
+  }
+};
 
 module.exports = connection;
 // connection()
