4cb4cd68585c2a1238b8ecf2aae3a4bc2d426bf2
rishidha <rishidha04@gmail.com>
2024-04-03 00:22:56 +0530

    updated

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -2,28 +2,26 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 const User = require("../models/user");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const connection = async (req, res) => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://rishidha04:ILu5sLmjsqim7AQy@cluster0.rtbw5ul.mongodb.net/team-app"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Successfully connected to MongoDB Atlas");
 
-    // Read data from JSON file
-    // const jsonData = fs.readFileSync(
-    //   path.resolve(__dirname, "data.json"),
-    //   "utf-8"
-    // );
-    // const data = JSON.parse(jsonData);
+    const jsonData = fs.readFileSync(
+      path.resolve(__dirname, "data.json"),
+      "utf-8"
+    );
+    const data = JSON.parse(jsonData);
 
-    // // Insert data into the database
-    // await User.insertMany(data);
-    // console.log("Data imported successfully.");
+    await User.insertMany(data);
+    console.log("Data imported successfully.");
   } catch (e) {
     console.log(`Error importing data to MongoDB Atlas: ${e.message}`);
   }
 };
 
 module.exports = connection;
-// connection()
