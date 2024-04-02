c21b4585ec8ed0c166736ce4d2fe0b24763e2fe9
rishidha <rishidha04@gmail.com>
2024-04-03 00:26:18 +0530

    updated

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -10,15 +10,6 @@ const connection = async (req, res) => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Successfully connected to MongoDB Atlas");
-
-    const jsonData = fs.readFileSync(
-      path.resolve(__dirname, "data.json"),
-      "utf-8"
-    );
-    const data = JSON.parse(jsonData);
-
-    await User.insertMany(data);
-    console.log("Data imported successfully.");
   } catch (e) {
     console.log(`Error importing data to MongoDB Atlas: ${e.message}`);
   }
